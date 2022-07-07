7dc50ec461f6203999eb22d8414a5dc2139550a1
ShaunLWM <shaunlimweimin@gmail.com>
2022-07-07 22:19:22 +0800

    Initial commit

---
+++ packages/server/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/server',
+};
---
+++ packages/server/src/main.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(8081, () => {
+  console.log(`Help`);
+});
