5851726771336eeeb82dd221266dbae7116efafc
ShaunLWM <shaunlimweimin@gmail.com>
2022-08-09 20:11:04 +0800

    Update tx list bgColor

--- client/src/components/TransactionList.tsx
+++ client/src/components/TransactionList.tsx
@@ -9,9 +9,9 @@ export function TransactionList(props: Props) {
   const { txs } = props;
 
   const renderTxRow = (tx: TransactionRecord, i: number) => {
-    // if tx.side === "ask", means user is selling and someone bought from him - so it's #b3f291 green since someone bought
+    // if tx.side === "ask", means user is selling and someone bought from him - so it's #f29191 red since the user successfully sold
     return (
-      <Flex flexDir="row" minWidth="400px" key={`${i}-${tx.time}-${tx.txId}`} bgColor={tx.side === "ask" ? "#b3f291" : "#f29191"}>
+      <Flex flexDir="row" minWidth="400px" key={`${i}-${tx.time}-${tx.txId}`} bgColor={tx.side === "ask" ? "#f29191" : "#b3f291"}>
         <Box flexBasis="100px">${tx.price.toFixed(2)}</Box>
         <Box flexBasis="100px">{tx.quantity}</Box>
         <Box>{formatTime(tx.time)}</Box>
